adcfeb5d7e15f06a91093360663721d2c7b7ea52
sunilb <pavanksunil@gmail.com>
2025-06-21 17:33:43 +0530

    fix: resolve jwt sign type error

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,14 +1,14 @@
-import jwt from 'jsonwebtoken';
+import jwt, { Secret } from 'jsonwebtoken';
 import config from '../config';
 
 export const generateToken = (payload: Record<string, unknown>): string => {
-  return jwt.sign(payload, config.jwt.secret as string, {
+  return jwt.sign(payload, config.jwt.secret as Secret, {
     expiresIn: config.jwt.expires_in,
   });
 };
 
 export const verifyToken = (token: string): Record<string, unknown> => {
-  return jwt.verify(token, config.jwt.secret as string) as Record<
+  return jwt.verify(token, config.jwt.secret as Secret) as Record<
     string,
     unknown
   >;
